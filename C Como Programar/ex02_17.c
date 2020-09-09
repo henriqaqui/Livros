fa673ffb6c11e9a015c36802761f7f77ae32cec8
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-09 18:29:48 -0300

    Create ex02_17.c

---
+++ C Como Programar/ex02_17.c
@@ -0,0 +1,25 @@
+/*
+C: COMO PROGRAMAR - EDIÇÃO 6
+2.17 Imprimindo valores com printf. Escreva um programa que imprima os números de 1 a 4 na mesma linha.
+Escreva o programa utilizando os métodos a seguir.
+    a) Uma instrução printf sem especificadores de conversão.
+    b) Uma instrução printf com quatro especificadores de conversão.
+    c) Quatro instruções printf.
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int um = 1, dois = 2, tres = 3, quatro = 4;
+
+    printf("1 2 3 4\n");
+
+    printf("%d %d %d %d\n", um, dois, tres, quatro);
+    
+    printf("1 ");
+    printf("2 ");
+    printf("3 ");
+    printf("4\n");
+    
+    return 0;
+}
