aee92354784e148825bcde5d163b0785bc616f73
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-09 12:09:36 -0300

    Create ex02_16.c

---
+++ C Como Programar/ex02_16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+    int num1, num2;
+    int soma, produto, diferenca, quociente, modulo;
+
+    printf("Este programa solicita dois numeros inteiros\ne apresenta na saida a SOMA, PRODUTO, DIFERENCA, QUOCIENTE e MODULO.\n\n");
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num2);
+    
+    soma = num1 + num2;
+    produto = num1 * num2;
+    diferenca = num1 - num2;
+    quociente = num1 / num2;
+    modulo = num1 % num2;
+
+    printf("%d + %d = %d\n", num1, num2, soma);
+    printf("%d * %d = %d\n", num1, num2, produto);
+    printf("%d - %d = %d\n", num1, num2, diferenca);
+    printf("%d / %d = %d\n", num1, num2, quociente);
+    printf("%d %% %d = %d\n", num1, num2, modulo);
+
+    return 0;
+}
