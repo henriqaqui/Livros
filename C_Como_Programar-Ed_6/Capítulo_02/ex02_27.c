6e9b341b043104b1f93cb480e9892e147ec6277a
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-10 17:45:36 -0300

    Create ex02_27.c

---
+++ C_Como_Programar-Ed_6/Capítulo_02/ex02_27.c
@@ -0,0 +1,28 @@
+/*
+=======================================================================================================================
+Filename:       ex02_27.c
+Author:         Henrique Rodrigues
+Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
+                2.27 Padrão de asteríscos alternados. Apresente o seguinte padrão de asteríscos alternados com oito
+                instruções printf, e depois apresente o mesmo padrão com o mínimo de instruções printf possível.
+Date:           10/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n");
+    printf("* * * * * * * *\n");
+    printf(" * * * * * * * *\n\n");
+    
+    printf("* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n"
+        "* * * * * * * *\n * * * * * * * *\n* * * * * * * *\n * * * * * * * *\n");
+    return 0;
+}
