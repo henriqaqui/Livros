ac6cb76acef78ebc9205dff64d7dfe7601a5f481
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-10 10:41:56 -0300

    Create ex02_21.c

---
+++ C Como Programar/ex02_21.c
@@ -0,0 +1,30 @@
+/*
+C: COMO PROGRAMAR - EDIÇÃO 6
+2.21 Formas com asteriscos. Escreva um programa que imprima as seguintes formas com asteríscos.
+    *********     ***       *         *
+    *       *   *     *    ***      *   *
+    *       *  *       *  *****    *     *
+    *       *  *       *    *     *       *
+    *       *  *       *    *    *         *
+    *       *  *       *    *     *       *
+    *       *  *       *    *      *     *
+    *       *   *     *     *       *   *
+    *********     ***       *         *
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    printf( "*********     ***       *         *\n" ); 
+    printf( "*       *   *     *    ***      *   *\n" );
+    printf( "*       *  *       *  *****    *     *\n" );
+    printf( "*       *  *       *    *     *       *\n" );
+    printf( "*       *  *       *    *    *         *\n" );
+    printf( "*       *  *       *    *     *       *\n" );
+    printf( "*       *  *       *    *      *     *\n" );
+    printf( "*       *   *     *     *       *   *\n" );
+    printf( "*********     ***       *         *\n" );
+
+    return 0;
+}
