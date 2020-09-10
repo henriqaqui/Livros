865d54cbc3cb6ce56d6dc1308b46ffd97752f3dc
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-10 17:15:32 -0300

    add

---
+++ C_Como_Programar-Ed_6/Capítulo_02/ex02_24.c
@@ -0,0 +1,29 @@
+/*
+=======================================================================================================================
+Filename:       ex02_24.c
+Author:         Henrique Rodrigues
+Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
+                2.24 Par ou ímpar. Escreva um programa que leia um inteiro, determine e imprima se ele é par ou ímpar.
+                [Dica: use o operador módulo. Um número par é um múltiplo de dois. Qualque múltiplo de dois fera resto zero quando dividido por dois.]
+Date:           10/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main(void)
+{   
+    int numero;
+
+    printf("Entre com um numero inteiro:\n");
+    scanf("%d", &numero);
+
+    if(numero % 2 == 0){
+        printf("O numero %d e PAR.\n", numero);
+    }
+    else{
+        printf("O numero %d e IMPAR.\n", numero);
+    }
+
+    return 0;
+}
---
+++ C_Como_Programar-Ed_6/Capítulo_02/ex02_25.c
@@ -0,0 +1,33 @@
+/*
+=======================================================================================================================
+Filename:       ex02_25.c
+Author:         Henrique Rodrigues
+Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
+                2.25 Imprima as suas iniciais em letras em bloco no sentido vertical da página. Construa cada letra em bloco a partir da letra que ele representa, como mostramos a seguir.
+                    PPPPPPPPP
+                         P  P
+                         P  P
+                         P  P
+                          PP
+Date:           10/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("HHHHHHHHH\n");
+    printf("    H\n");
+    printf("    H\n");
+    printf("    H\n");
+    printf("HHHHHHHHH\n\n");
+
+    printf("RRRRRRRRR\n");
+    printf("   R R  R\n");
+    printf("  R  R  R\n");
+    printf(" R   R  R\n");
+    printf("R     RR\n");
+    
+    return 0;
+}
