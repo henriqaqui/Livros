18426be6f53f34559787522c6349d1ce0641b47a
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-11 09:53:43 -0300

    Create C02_Ex12.c

---
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_2/C02_Ex12.c
@@ -0,0 +1,20 @@
+/*
+=======================================================================================================================
+Filename:       C02_Ex12.c
+Author:         Henrique Rodrigues
+Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
+                12 elabore um programa que contenha uma constante qualquer do tipo int. Use o comando const.
+                Imprima essa constante.
+Date:           11/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main(void){
+    const int ANO = 2020;
+
+    printf("%d\n", ANO);
+
+    return 0;
+}
