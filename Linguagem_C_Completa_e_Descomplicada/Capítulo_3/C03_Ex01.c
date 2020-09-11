ed98aaeaab59b84aff82660524f11e2ba3c59f5e
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-11 11:39:52 -0300

    Create C03_Ex01.c

---
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_3/C03_Ex01.c
@@ -0,0 +1,21 @@
+/*
+=======================================================================================================================
+Filename:       C03_Ex01.c
+Author:         Henrique Rodrigues
+Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
+                1 Faça um programa que leia um número inteiro e retorne seu antecessor e seu sucessor.
+Date:           11/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main(){
+    int numero;
+
+    scanf("%d", &numero);
+
+    printf("%d\n%d\n", numero -1, numero + 1);
+
+    return 0;
+}
