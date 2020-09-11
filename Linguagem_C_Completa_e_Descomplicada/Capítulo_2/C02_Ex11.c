f736cdf1305a493fb2aeba00af43e41861434cfb
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-11 09:38:18 -0300

    Create C02_Ex11.c

---
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_2/C02_Ex11.c
@@ -0,0 +1,21 @@
+/*
+=======================================================================================================================
+Filename:       C02_Ex11.c
+Author:         Henrique Rodrigues
+Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
+                11 Elabore um programa que contenha uma constante qualquer do tipo float. Use o comando #define.
+                Imprima essa constante.
+Date:           11/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+#define PI 3.1415F
+
+int main(void){
+
+    printf("%f\n", PI);
+
+    return 0;
+}
