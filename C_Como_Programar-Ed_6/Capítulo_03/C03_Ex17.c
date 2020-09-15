521f13eb8924af4555cacbe8b1822050f301a03f
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-15 17:58:56 -0300

    Create C03_Ex17.c

---
+++ C_Como_Programar-Ed_6/Capítulo_03/C03_Ex17.c
@@ -0,0 +1,50 @@
+/*
+=======================================================================================================================
+Filename:       C03_Ex17.c
+Author:         Henrique Rodrigues
+Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
+                2.17 Consumo de gasolina. Os motoristas se preocupam com o consumo de gasolina de seus automóveis.
+                Um motorista manteve um registro do número de abastecimento que fez, registrando também o número de
+                quilômetros rodados e de litros obtidos a cada abastecimento. Desenvolva um programa que peça o número
+                de quilômetros dirigidos e a quantidade de litros obtidos a cada abastecimento. O programa deverá calcular e exibir a quantidade de quilômetros rodados por litros usados. Depois de processar toda a
+                informação, o programa deverá calcular e exibir o total combinado de quilômetros por litro para todos
+                os abastecimentos.
+Date:           15/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    float km, litros, kmLitro;
+    float kmTotal = 0, litrosTotal = 0, kmLitroTotal = 0;
+
+    printf( "Informe quantos km dirigiu (-1 para terminar):\n" );
+    scanf( "%f", &km );
+
+    while ( -1 != km ){
+       printf( "Informe quantos litros abasteceu:\n" );
+       scanf( "%f", &litros );
+
+       kmLitro = km / litros;
+
+       kmTotal += km;
+       litrosTotal += litros;
+       
+       printf( "O consumo atual e de: %f km/l\n\n", kmLitro );
+
+       printf( "Informe quantos km dirigiu (-1 para terminar):\n" );
+       scanf( "%f", &km );
+    }
+
+    if( 0 != kmTotal ){
+        kmLitroTotal = kmTotal / litrosTotal;
+        printf( "O consumo geral foi de: %f km/l\n", kmLitroTotal );
+    }
+    else{
+        printf( "ERRO! Nenhum dado foi coletado.\n" );
+    }
+    
+    return 0;
+}
