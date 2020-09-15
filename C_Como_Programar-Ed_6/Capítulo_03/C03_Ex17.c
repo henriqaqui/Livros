f82f43553542d9176936f6b764c5416641d1a7d6
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-15 18:00:23 -0300

    Update C03_Ex17.c

--- C_Como_Programar-Ed_6/Capítulo_03/C03_Ex17.c
+++ C_Como_Programar-Ed_6/Capítulo_03/C03_Ex17.c
@@ -6,7 +6,8 @@ Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
                 2.17 Consumo de gasolina. Os motoristas se preocupam com o consumo de gasolina de seus automóveis.
                 Um motorista manteve um registro do número de abastecimento que fez, registrando também o número de
                 quilômetros rodados e de litros obtidos a cada abastecimento. Desenvolva um programa que peça o número
-                de quilômetros dirigidos e a quantidade de litros obtidos a cada abastecimento. O programa deverá calcular e exibir a quantidade de quilômetros rodados por litros usados. Depois de processar toda a
+                de quilômetros dirigidos e a quantidade de litros obtidos a cada abastecimento. O programa deverá
+                calcular e exibir a quantidade de quilômetros rodados por litros usados. Depois de processar toda a
                 informação, o programa deverá calcular e exibir o total combinado de quilômetros por litro para todos
                 os abastecimentos.
 Date:           15/09/2020
