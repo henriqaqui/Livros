32dbb13d9b59548dde2710e0c6107db89124b683
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-15 08:18:55 -0300

    Update ex02_16.c

--- C_Como_Programar-Ed_6/Capítulo_02/ex02_16.c
+++ C_Como_Programar-Ed_6/Capítulo_02/ex02_16.c
@@ -16,13 +16,13 @@ int main( void )
     int num1, num2;
     int soma, produto, diferenca, quociente, modulo;
 
-    printf("Este programa solicita dois numeros inteiros\n"
-        "e apresenta na saida a SOMA, PRODUTO, DIFERENCA, QUOCIENTE e MODULO.\n\n");
+    printf( "Este programa solicita dois numeros inteiros\n"
+        "e apresenta na saida a SOMA, PRODUTO, DIFERENCA, QUOCIENTE e MODULO.\n\n" );
     
-    printf("Digite o primeiro numero: ");
-    scanf("%d", &num1);
-    printf("Digite o segundo numero: ");
-    scanf("%d", &num2);
+    printf( "Digite o primeiro numero: " );
+    scanf( "%d", &num1 );
+    printf( "Digite o segundo numero: " );
+    scanf( "%d", &num2 );
     
     soma = num1 + num2;
     produto = num1 * num2;
@@ -30,11 +30,11 @@ int main( void )
     quociente = num1 / num2;
     modulo = num1 % num2;
 
-    printf("%d + %d = %d\n", num1, num2, soma);
-    printf("%d * %d = %d\n", num1, num2, produto);
-    printf("%d - %d = %d\n", num1, num2, diferenca);
-    printf("%d / %d = %d\n", num1, num2, quociente);
-    printf("%d %% %d = %d\n", num1, num2, modulo);
+    printf( "%d + %d = %d\n", num1, num2, soma );
+    printf( "%d * %d = %d\n", num1, num2, produto );
+    printf( "%d - %d = %d\n", num1, num2, diferenca );
+    printf( "%d / %d = %d\n", num1, num2, quociente );
+    printf( "%d %% %d = %d\n", num1, num2, modulo );
 
     return 0;
 }
