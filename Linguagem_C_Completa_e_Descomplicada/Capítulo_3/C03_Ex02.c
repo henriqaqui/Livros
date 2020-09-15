ff4eb579c84c3c18af664d79e40a00574eb7d273
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-15 10:10:04 -0300

    Update C03_Ex02.c

--- Linguagem_C_Completa_e_Descomplicada/Capítulo_3/C03_Ex02.c
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_3/C03_Ex02.c
@@ -4,7 +4,7 @@ Filename:       C03_Ex02.c
 Author:         Henrique Rodrigues
 Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
                 2 Faça um programa que leia um número real e imprima a quinta parte desse número.
-Date:           11/09/2020
+Date:           15/09/2020
 =======================================================================================================================
 */
 
