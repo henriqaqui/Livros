e84103b6c398e661409dc0a2015709f812cde236
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-16 16:36:58 -0300

    Update C03_Ex17.c

--- C_Como_Programar-Ed_6/Capítulo_03/C03_Ex17.c
+++ C_Como_Programar-Ed_6/Capítulo_03/C03_Ex17.c
@@ -44,7 +44,7 @@ int main( void )
         printf( "O consumo geral foi de: %f km/l\n", kmLitroTotal );
     }
     else{
-        printf( "ERRO! Dados insuficientes.\n" );
+        printf( "Dados insuficientes.\n" );
     }
     
     return 0;
