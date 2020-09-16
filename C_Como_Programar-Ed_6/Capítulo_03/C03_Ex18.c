d525ce1ddbe6a6ef88465e8cfa789354c60cb839
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-16 15:42:21 -0300

    Create C03_Ex18.c

---
+++ C_Como_Programar-Ed_6/Capítulo_03/C03_Ex18.c
@@ -0,0 +1,24 @@
+/*
+=======================================================================================================================
+Filename:       C03_Ex18.c
+Author:         Henrique Rodrigues
+Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
+                3.18 Consumo de gasolina. Os motoristas se preocupam com o consumo de gasolina de seus automóveis.
+                Um motorista manteve um registro do número de abastecimento que fez, registrando também o número de
+                quilômetros rodados e de litros obtidos a cada abastecimento. Desenvolva um programa que peça o número
+                de quilômetros dirigidos e a quantidade de litros obtidos a cada abastecimento. O programa deverá
+                calcular e exibir a quantidade de quilômetros rodados por litros usados. Depois de processar toda a
+                informação, o programa deverá calcular e exibir o total combinado de quilômetros por litro para todos
+                os abastecimentos.
+Date:           16/09/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    
+    
+    return 0;
+}
