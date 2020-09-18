a8a8bf939a9c71d1c0a6638b37665d866831e190
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-18 10:51:52 -0300

    Update C04_Ex09.c

--- C_Como_Programar-Ed_6/Capítulo_04/C04_Ex09.c
+++ C_Como_Programar-Ed_6/Capítulo_04/C04_Ex09.c
@@ -6,7 +6,8 @@ Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
                 4.9 Soma de uma sequência de inteiros. Escreva um programa que some uma sequência de inteiros.
                 Considere que o primeiro inteiro lido com scanf especifique o número de valores restantes a serem
                 inseridos. Seu programa deve ler apenas um valor toda vez que scanf for executado. Uma sequência
-                de entrada típica poderia ser 5 100 200 300 400 500, onde 5 indica que os cinco valores subsequentes devem ser somados.
+                de entrada típica poderia ser 5 100 200 300 400 500, onde 5 indica que os cinco valores subsequentes
+                devem ser somados.
 Date:           18/09/2020
 =======================================================================================================================
 */
