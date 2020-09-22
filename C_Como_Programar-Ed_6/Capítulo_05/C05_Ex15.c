55b0a6836840fd0e001c015c11c216fb4612126a
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-22 08:02:19 -0300

    Update C05_Ex15.c

--- C_Como_Programar-Ed_6/Capítulo_05/C05_Ex15.c
+++ C_Como_Programar-Ed_6/Capítulo_05/C05_Ex15.c
@@ -6,7 +6,7 @@ Description:    C: COMO PROGRAMAR - 6ª EDIÇÃO - PORTUGUÊS
                 5.15 Cálculos de hipotenusa. Defina uma função chamada hipotenusa que calcule o tamanho da hipotenusa
                 de um triângulo retângulo quando os outros dois lados são conhecidos. Use essa função em um programa
                 para determinar o tamanho da hipotenusa de cada um dos triângulos da tabela a seguir. A função deverá
-                usar dois argumentos do tipo double e retornar a hipotenusa como um double. 
+                usar dois argumentos do tipo double e retornar a hipotenusa como um double.
 Date:           22/09/2020
 =======================================================================================================================
 */
@@ -23,7 +23,7 @@ int main( void )
     scanf( "%lf %lf", &ladoA, &ladoB );
     hipotenusa( ladoA, ladoB );
 
-    return 0; 
+    return 0;
 }
 
 
@@ -31,9 +31,9 @@ void hipotenusa ( double x, double y )
 {
     double hip;
 
-    hip = sqtr( pow( x, 2 ) + pow( y, 2 ) );
+    hip = sqrt( pow( x, 2 ) + pow( y, 2 ) );
 
-    printf( "%f\n", hip );
+    printf( "%.2f\n", hip );
 
     return;
 }
