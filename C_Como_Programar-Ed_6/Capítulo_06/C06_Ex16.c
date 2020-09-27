caf63d8bdc856b538f553ccd3f2c5e1ebcf31c11
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-09-27 17:14:42 -0300

    Update C06_Ex16.c

--- C_Como_Programar-Ed_6/Capítulo_06/C06_Ex16.c
+++ C_Como_Programar-Ed_6/Capítulo_06/C06_Ex16.c
@@ -14,19 +14,16 @@ Date:           26/09/2020
 #include <stdio.h>
 #define SIZE 99
 
-/* function prototypes */
 void mean( const int answer[] );
 void median( int answer[] );
 void mode( int freq[], const int answer[] ) ;
 void bubbleSort( int a[] );
 void printArray( const int a[] );
 
-/* function main begins program execution */
 int main( void )
 {
-   int frequency[ 10 ] = { 0 }; /* initialize array frequency */
+   int frequency[ 10 ] = { 0 }; 
 
-   /* initialize array response */
    int response[ SIZE ] =
       { 6, 7, 8, 9, 8, 7, 8, 9, 8, 9,
         7, 8, 9, 5, 9, 8, 7, 8, 7, 8,
@@ -39,25 +36,23 @@ int main( void )
         7, 4, 4, 2, 5, 3, 8, 7, 5, 6,
         4, 5, 6, 1, 6, 5, 7, 8, 7 };
 
-   /* process responses */
    mean( response );
    median( response );
    mode( frequency, response );
-   return 0; /* indicates successful termination */
-} /* end main */
+   return 0; 
+} 
 
-/* calculate average of all response values */
+
 void mean( const int answer[] )
 {
-   int j; /* counter for totaling array elements */
-   int total = 0; /* variable to hold sum of array elements */
+   int j;
+   int total = 0;
 
    printf( "%s\n%s\n%s\n", "********", "  Mean", "********" );
 
-   /* total response values */
    for ( j = 0; j < SIZE; j++ ) {
       total += answer[ j ];
-   } /* end for */
+   } 
 
    printf( "The mean is the average value of the data\n"
            "items. The mean is equal to the total of\n"
@@ -65,25 +60,24 @@ void mean( const int answer[] )
            "of data items ( %d ). The mean value for\n"
            "this run is: %d / %d = %.4f\n\n",
            SIZE, total, SIZE, (  double  ) total / SIZE );
-} /* end function mean */
+}
 
-/* sort array and determine median element's value */
+/* ordena array e determina valor do elemento mediano */
 void median( int answer[] )
 {
    printf( "\n%s\n%s\n%s\n%s",
            "********", " Median", "********",
            "The unsorted array of responses is" );
 
-   printArray( answer ); /* output unsorted array */
+   printArray( answer ); /* exibe array não ordenado */
 
-   bubbleSort( answer ); /* sort array */
+   bubbleSort( answer ); /* ordena array */
 
    printf( "\n\nThe sorted array is" );
-   printArray( answer ); /* output sorted array */
+   printArray( answer ); /* exibe array ordenado */
 
-   /* display median element*/
-   /* Muda a resposta caso SIZE seja par */
-   if( SIZE % 2 == 0 ){
+   /* exibe elemento mediano*/
+   if( SIZE % 2 == 0 ){   /* Muda a resposta caso SIZE seja par */
        printf( "\n\nA mediana e a media dos elementos %d e %d of\n"
                "the sorted %d element array.\n"
                "For this run the median is %.1f\n\n",
@@ -95,94 +89,89 @@ void median( int answer[] )
                "For this run the median is %d\n\n",
                SIZE / 2, SIZE, answer[ SIZE / 2 ] );
    }
-} /* end function median */
+} /* fim da função median */
 
-/* determine most frequent response */
+/* determina a resposta mais frequente */
 void mode( int freq[], const int answer[] )
 {
-   int rating; /* counter for accessing elements 1-9 of array freq */
-   int j; /* counter for summarizing elements 0-98 of array answer */
-   int h; /* counter for diplaying histograms of elements in array freq */
-   int largest = 0; /* represents largest frequency */
-   int modeValue = 0; /* represents most frequent response */
+   int rating; /* contador para acessar os elementos 1- 9 do array freq */
+   int j; /* contador para resumir os elementos 0 - 98 do array answer */
+   int h; /* contador para exibir histogramas dos elementos no array freq */
+   int largest = 0; /* representa maior frequencia */
+   int modeValue = 0; /* representa resposta mais frequente */
 
    printf( "\n%s\n%s\n%s\n",
            "********", "  Mode", "********" );
 
-   /* initialize frequencies to 0 */
+   /* inicializa frequências em 0 */
    for ( rating = 1; rating <= 9; rating++ ) {
       freq[ rating ] = 0;
-   } /* end for */
+   } /* fim do for */
 
-   /* summarize frequencies */
+   /* frequência de resumo */
    for ( j = 0; j < SIZE; j++ ) {
       ++freq[ answer[ j ] ];
-   } /* end for */
+   } /* fim do for */
 
-   /* output headers for result columns */
+   /* cabeçalhos de impressão para colunas do resultado */
    printf( "%s%11s%19s\n\n%54s\n%54s\n\n",
            "Response", "Frequency", "Histogram",
            "1    1    2    2", "5    0    5    0    5" );
 
-   /* output results */
+   /* exibe resultados */
    for ( rating = 1; rating <= 9; rating++ ) {
       printf( "%8d%11d          ", rating, freq[ rating ] );
 
-      /* keep track of mode value and largest frequency value */
+      /* acompanha valor da moda e valor da maior frequência */
       if ( freq[ rating ] > largest ) {
          largest = freq[ rating ];
          modeValue = rating;
-      } /* end if */
+      } /* fim do if */
 
-      /* output histogram bar representing frequency value */
+      /* barra de histograma de saída de impressão que representa valor de frequência */
       for ( h = 1; h <= freq[ rating ]; h++ ) {
          printf( "*" );
-      } /* end inner for */
+      } /* fim do for interno */
 
-      printf( "\n" ); /* being new line of output */
-   } /* end outer for */
+      printf( "\n" );
+   } /* fim do for externo */
 
-   /* display the mode value */
+   /* exibe o valor da moda */
    printf( "The mode is the most frequent value.\n"
            "For this run the mode is %d which occurred"
            " %d times.\n", modeValue, largest );
-} /* end function mode */
+} /* fim da função mode */
 
-/* function that sorts an array with bubble sort algorithm */
 void bubbleSort( int a[] )
 {
-   int pass; /* pass counter */
-   int j; /* comparison counter */
-   int hold; /* temporary location used to swap elements */
+   int pass; 
+   int j; 
+   int hold; 
 
-   /* loop to control number of passes */
    for ( pass = 1; pass < SIZE; pass++ ) {
 
-      /* loop to control number of comparisons per pass */
       for ( j = 0; j < SIZE - 1; j++ ) {
 
-         /* swap elements if out of order */
          if ( a[ j ] > a[ j + 1 ] ) {
             hold = a[ j ];
             a[ j ] = a[ j + 1 ];
             a[ j + 1 ] = hold;
-         } /* end if */
-      } /* end inner for */
-   } /* end outer for */
-} /* end function bubbleSort */
+         } 
+      }
+   } 
+} 
 
-/* output array contents (20 values per row) */
+
 void printArray( const int a[] )
 {
-   int j; /* counter */
+   int j; 
 
-   /* output array contents */
    for ( j = 0; j < SIZE; j++ ) {
 
-      if ( j % 20 == 0 ) { /* begin new line every 20 values */
+      if ( j % 20 == 0 ) { 
          printf( "\n" );
-      } /* end if */
+      }
 
       printf( "%2d", a[ j ] );
-   } /* end for */
-} /* end function printArray */
+   } 
+}
