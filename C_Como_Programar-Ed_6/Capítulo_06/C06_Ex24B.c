19eb43574a1ed4beead1f6f60fc68911f08cc0b0
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-10-01 11:50:22 -0300

    Update C06_Ex24B.c

--- C_Como_Programar-Ed_6/Capítulo_06/C06_Ex24B.c
+++ C_Como_Programar-Ed_6/Capítulo_06/C06_Ex24B.c
@@ -87,26 +87,22 @@ int validaMovimentos( int tab[8][8], int *linha, int *coluna, int mov )
             move( linha, coluna, mov, 1 );
 
             if( ( *linha < 8 && *linha >= 0 ) && ( *coluna < 8 && *coluna >= 0 ) && ( 0  == tab[ *linha ] [ *coluna ] ) ){
-            move( linha, coluna, mov, 0 );
-
-                    return 1;
+                move( linha, coluna, mov, 0 );
+                return 1;
             }
             else{
                 move( linha, coluna, mov, 0 );
-
             }      
         }
     }
     else{
         move( linha, coluna, mov, 1 );
 
-
         if( ( *linha < 8 && *linha >= 0 ) && ( *coluna < 8 && *coluna >= 0 ) && ( 0  == tab[ *linha ] [ *coluna ] ) ){
             return 1;
         }
         else{
             move( linha, coluna, mov, 0 );
-
         }    
     }
 
