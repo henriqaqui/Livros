b998a012bada229d304d06fe2d94075ec783033d
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-10-05 15:11:54 -0300

    Create C05_Ex02.c

---
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_05/C05_Ex02.c
@@ -0,0 +1,28 @@
+/*
+=======================================================================================================================
+Filename:       C05_Ex01.c
+Author:         Henrique Rodrigues
+Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
+                1 Faça um programa que leia um número inteiro positivo N e imprima todos os números naturais de N até 0
+                em ordem decrescente.
+Date:           05/10/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    int numero, contador = 0;
+
+    scanf( "%d", &numero );
+
+    for( contador = numero; contador >= 0; contador-- ){
+        printf( "%d\t", contador );
+        if( contador % 10 == 0 ){
+            printf( "\n" );
+        }
+    }
+
+    return 0;
+}
