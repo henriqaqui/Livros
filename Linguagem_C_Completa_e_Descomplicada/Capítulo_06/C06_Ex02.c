a0f8f83a82729750e81aae493d98e86070ae5b99
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-10-06 11:11:19 -0300

    Create C06_Ex02.c

---
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_06/C06_Ex02.c
@@ -0,0 +1,29 @@
+/*
+=======================================================================================================================
+Filename:       C06_Ex02.c
+Author:         Henrique Rodrigues
+Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
+                2 Faça um program que leia uma string e imprima as quatro primeiras letras dela.
+Date:           06/10/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+#define LIMITE 4
+
+int main( void )
+{
+    char nome[30];
+    int i;
+
+    scanf( "%s", nome );
+
+    for ( i = 0; i < LIMITE; i++ ){
+        printf( "%c", nome[i] );
+    }
+    
+    printf( "\n" );
+    
+    return 0;
+}
