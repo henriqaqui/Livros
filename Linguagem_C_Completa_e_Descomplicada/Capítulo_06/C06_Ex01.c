58cee8804e65369b66609e64ca67be29f94fd5f0
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-10-06 11:11:13 -0300

    Create C06_Ex01.c

---
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_06/C06_Ex01.c
@@ -0,0 +1,21 @@
+/*
+=======================================================================================================================
+Filename:       C06_Ex01.c
+Author:         Henrique Rodrigues
+Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
+                1 Faça um programa que leia uma string e a imprima na tela.
+Date:           06/10/2020
+=======================================================================================================================
+*/
+
+#include <stdio.h>
+
+int main( void )
+{
+    char nome[30];
+
+    scanf( "%s", nome );
+    printf( "%s", nome );
+
+    return 0;
+}
