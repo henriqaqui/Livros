b2f20314d30d77ac77f9a0fffa9145f07a605234
henriqaqui <55706879+henriqaqui@users.noreply.github.com>
2020-10-06 10:14:27 -0300

    Update C05_Ex02.c

--- Linguagem_C_Completa_e_Descomplicada/Capítulo_05/C05_Ex02.c
+++ Linguagem_C_Completa_e_Descomplicada/Capítulo_05/C05_Ex02.c
@@ -1,9 +1,9 @@
 /*
 =======================================================================================================================
-Filename:       C05_Ex01.c
+Filename:       C05_Ex02.c
 Author:         Henrique Rodrigues
 Description:    LINGUAGEM C COMPLETA E DESCOMPLICADA - 2ª EDIÇÃO
-                1 Faça um programa que leia um número inteiro positivo N e imprima todos os números naturais de N até 0
+                2 Faça um programa que leia um número inteiro positivo N e imprima todos os números naturais de N até 0
                 em ordem decrescente.
 Date:           05/10/2020
 =======================================================================================================================
